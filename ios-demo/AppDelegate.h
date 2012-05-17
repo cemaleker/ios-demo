04fbba3196c923af264e741053c3ffbfdc74603c
Cemal Eker <hello@cemaleker.com>
2012-05-17 22:55:33 +0300

    Initial commit

---
+++ ios-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ios_demoAppDelegate.h
+//  ios-demo
+//
+//  Created by Bal on 5/17/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ ios-demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ios-demo
+//
+//  Created by Bal on 5/17/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, @"AppDelegate", nil);
+    [pool release];
+    return retVal;
+}
